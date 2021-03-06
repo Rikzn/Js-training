b6fda03fabfba6a8a3f56c2ecd4a731c94120496
Rikzn <41932726+Rikzn@users.noreply.github.com>
2021-03-06 13:26:20 +0300

    Lesson-2-next

--- lesson_1/main.js
+++ lesson_1/main.js
@@ -20,5 +20,61 @@ let one = document.getElementById('one').innerHTML=('Hello world!');
 let two = document.getElementById('two').innerHTML=(12 * 12);
 
 // Task 8
+let oneClass = document.querySelector('.one').innerHTML=('Hello World');
 
-let oneClass = document.querySelector
+// Task 9
+let h2 = document.querySelector('h2').innerHTML=("<h2>Hello <span>world</span></h2>");
+
+// Task 10
+let three = document.querySelector('.three').innerHTML=('<h3>Hello <span>Rishat</span></h3>');
+
+// Task 11
+let four = document.querySelector('.four').innerHTML=('<h4>Привет</h4> <p>Параграф</p>');
+
+// Task 12
+let a = document.querySelector('.five');
+a.innerHTML = 3.1415;
+
+// Task 13
+let div7 = document.querySelector('.seven');
+div7.innerHTML = ('<img src="https://cdn4.iconfinder.com/data/icons/food-and-drink-flat-gradient/32/cone_ice_cream_food_drink_sweet-512.png" alt="">');
+
+// Task 14
+let task14 = document.querySelector('.task14');
+let z1 = 6,
+    z2 = 3;
+task14.innerHTML = z1 + z2;
+
+// Task 15
+let task15 = document.querySelector('.task15');
+let y1 = 6,
+    y2 = 3;
+task15.innerHTML = y1 / y2;
+
+// Task 16
+let task16 = document.querySelector('.task16');
+let x1 = 'hello',
+    x2 = 5;
+    task16.innerHTML = x1 + x2;
+
+
+// Task 17
+let d1 = document.querySelector('.test-1');
+console.log(d1);
+
+// Task 18
+let d2 = document.querySelector('.test-2');
+console.log(d2);
+d2 = 5;
+console.log(d2);
+
+// Task 19
+divS3 = document.querySelector('.s3');
+console.log(divS3);
+divS3 = document.querySelector('.s4');
+console.log(divS3);
+
+// Task 20
+
+let body = document.querySelector('body');
+// body.innerHTML = '';
---
+++ lesson_2/main.js
@@ -0,0 +1,73 @@
+// Task 1 
+let a = 7,
+    b = 9;
+    console.log(a * b);
+
+// Task 2
+let c = 7,
+    d = 9;
+    console.log(c / d);
+
+// Task 3
+let e = 3,
+    f = 5;
+    console.log(e + f);
+
+// Task 4
+let e1 = "3",
+    f1 = 5;
+    console.log(e1 + f1);
+
+// Task 5
+let e2 = 3,
+    f2 = 0;
+    console.log(e2 / f2);
+
+// Task 6
+let e3 = 3,
+    f3 = "hello";
+console.log(e3 + f3);
+
+// Task 7
+let e4 = 3,
+    f4 = "hello";
+console.log(e3 * f3);
+
+// Task 8
+let input = document.querySelector('.input');
+let btn = document.querySelector('.btn');
+// console.log(input);
+// btn.addEventListener('click', () => {
+//     console.log(input.value);
+// });
+
+// Task 9
+let text = document.querySelector('.text');
+// btn.addEventListener('click', () => {
+//     text.innerHTML = input.value;
+//     input.value = '';
+// });
+
+// Task 10
+// btn.addEventListener('click', () => {
+//     text.innerHTML = +input.value * 10;
+//     input.value = '';
+// });
+
+
+// Task 11
+// btn.addEventListener('click', () => {
+//     text.innerHTML = +input.value + 10;
+//     input.value = '';
+// });
+
+// Task 12
+let input1 = document.querySelector('.input-1'),
+    input2 = document.querySelector('.input-2');
+    btn.addEventListener('click', () => {
+        console.log('Hello' + ' ' + input1.value + ' ' + input2.value);
+        input.value = '';
+    });
+
+
+// Task 13
