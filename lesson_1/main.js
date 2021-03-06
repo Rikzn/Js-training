fee63b0c7766095e224921a2dbe68dd7cfc293cc
rishat <hrrkzn@yandex.ru>
2021-03-06 09:30:45 +0300

    lesson-1

---
+++ lesson_1/main.js
@@ -0,0 +1,24 @@
+// Task 1
+console.log('Rishat');
+
+// Task 2
+console.log(02);
+
+// Task 3
+console.log('Добро '+'пожаловать '+' на курс');
+
+// Task 4
+// alert(2019);
+
+// Task 5
+// alert(2019 - 200);
+
+// Task 6
+let one = document.getElementById('one').innerHTML=('Hello world!');
+
+// Task 7
+let two = document.getElementById('two').innerHTML=(12 * 12);
+
+// Task 8
+
+let oneClass = document.querySelector
